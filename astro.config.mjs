c7127e866629ed8a33cba59808dac9bd8c30387d
Tomorrowdawn <tomorrowdawn@mail.ustc.edu.cn>
2026-05-10 16:10:46 +0800

    Revert "Add Keystatic CMS with better editing experience"
    This reverts commit 23db355f861f97cb4cb5daf4e24fb6509703182d.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,12 @@ import { defineConfig } from "astro/config";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 import sitemap from "@astrojs/sitemap";
-import keystatic from "@keystatic/astro";
 
 export default defineConfig({
   site: "https://blog.tomorrowdawn.cc",
   base: "",
 
-  integrations: [sitemap(), keystatic()],
+  integrations: [sitemap()],
 
   markdown: {
     remarkPlugins: [remarkMath],
